c45150207309c8b0ff607979f3f4a83a21eeadf5
FoobarIT <dan.demart@laposte.net>
2020-09-16 21:53:58 +0200

    :art: Review Nodar, Start by new navigation.

--- nodar.sh
+++ nodar.sh
@@ -4,117 +4,195 @@ if [ $wa != "root" ]; then
   echo "Start this script with sudo prefix."
   exit
 fi
-echo -e "\e[94m░▒▓[Nodar v0.1 Fix]▓▒░\e[0m"
-echo -e "\e[94mWe installing a simple environment to web app node.js & more.\e[0m"
+echo -e "\e[94m________░▒▓[Nodar v0.1 Fix]▓▒░________\e[0m"
+echo -e "\e[94mManage simply your Node.JS web app & tools.\e[0m"
 echo -e "\e[1mDeveloper: Dany .S, https://github.com/FoobarIT\e[0m"
 echo ""
-echo -e "We update your linux system"
-apt-get update
-apt-get upgrade
-apt-get clean
-apt-get autoremove --purge
-echo -e "\e[32mYour linux system is now update.\e[0m"
-echo ""
 
-node_Mode() {
-  echo "We install Node.JS"
-  sudo apt install nodejs npm > /dev/null
-  echo "We install build-essential"
-  sudo apt-get install -y build-essential
-}
-framework_Mode() {
-  echo -e "\e[1m[Framework Manager]\e[0m"
-  PS3='Choose an option:'
-  options=("vue-cli" "react.js" "angular.js" "adonis.js" "Exit")
-    select opt in "${options[@]}"
-      do
-        case $opt in
-          "vue-cli")
-            echo "vue-cli installation.."
-            sudo npm install -g @vue/
-            echo "Success Install"
-            ;;
-          "react.js")
-            echo "react.js installation.."
-            sudo npm install -g create-react-app
-            echo "Success Install"
-            ;;
-          "angular.js")
-            echo "angular.js installation.."
-            sudo npm install -g @angular/cli
-            echo "Success Install"
-            ;;
-          "adonis.js")
-            echo "adonis.js installation.."
-            sudo npm i -g @adonisjs/cli
-            echo "Success Install"
-            ;;
-          "Exit")
-            break
-            ;;
-          *) echo "[Err]Choice invalid - $REPLY";;
-        esac
-      done
-    exit 0
-}
 
-module_Mode() {
-  echo -e "\e[1m[Module manager]\e[0m"
-  PS3='Choose an option:'
-  options=("body-parser" "express" "socket.io" "axios" "Framework installation")
-    select opt in "${options[@]}"
-      do
-        case $opt in
-         "body-parser")
-           echo "body-parser installation.."
-           sudo npm install body-parser --save
-           echo "Success Install"
-           ;;
-         "express")
-           echo "express installation.."
-           sudo npm install express --save
-           echo "Success Install"
-           ;;
-         "socket.io")
-           echo "socket.io installation.."
-           sudo npm install socket.io --save
-           echo "Success Install"
-           ;;
-         "axios")
-           echo "axios installation.."
-           sudo npm install axios --save
-           echo "Success Install"
-           ;;
-         "Framework installation")
-           framework_Mode
-           ;;
-         *) echo "[Err]Choice invalid - $REPLY";;
-       esac
-     done
-}
 
-echo -e "\e[1m[Node.js manager]\e[0m"
-PS3='Choose an option:'
-options=("Install" "Update" "Check version" "Module installation")
-  select opt in "${options[@]}"
-    do
-      case $opt in
-        "Install")
-          node_Mode
-          ;;
-        "Update")
-          echo "update"
-          sudo n latest
-          ;;
-        "Check version")
-          echo "Node.JS version:"
-          node -v
-          echo "NPM version:"
-          npm -v
-          ;;
-        "Module installation")
-          module_Mode
-          ;;
-        *) echo "[Err]Choice invalid - $REPLY";;
-      esac
-  done
+
+menu_install_tools() {
+	echo -e "\e[1m[Tools Manager]\e[0m"
+	echo -e "\e[1mThis tools is installed in global."
+	echo ""
+
+	PS3='Choose an option:'
+	options=("Nodemon" "Pm2" "Grunt" "Karma" "Webpack" "Go menu =>")
+	  select opt in "${options[@]}"
+	    do
+	      case $opt in
+	        "Nodemon")
+	          	#Do any want
+	        ;;
+	        "Pm2")
+	          	# Do whatever
+	        ;;
+	        "Grunt")
+	          	# Do whatever
+	        ;;
+	        "Karma")
+	          	# Do whatever
+	        ;;
+	        "Webpack")
+	        	# Do
+	        ;;
+	        "Go menu =>")
+				menu_nodar
+			;;
+	        *) echo "[Err]Choice invalid - $REPLY";;
+	      esac
+	  done
+};
+menu_install_back() {
+	echo -e "\e[1m[Back End Framework]\e[0m"
+	echo -e "\e[1mInstall your favorite Framework."
+	echo ""
+
+	PS3='Choose an option:'
+	options=("Express" "Socket" "Fs" "Multer" "Go menu =>")
+	  select opt in "${options[@]}"
+	    do
+	      case $opt in
+	        "Express")
+	          	#Do any want
+	        ;;
+	        "Socket")
+	          	# Do whatever
+	        ;;
+	        "Fs")
+	          	# Do whatever
+	        ;;
+	        "Multer")
+			
+			;;
+	        "Go menu =>")
+				menu_nodar
+			;;
+	        *) echo "[Err]Choice invalid - $REPLY";;
+	      esac
+	  done
+};
+menu_install_data() {
+	echo -e "\e[1m[Data Sytem]\e[0m"
+	echo -e "\e[1mInstall your data system."
+	echo ""
+
+	PS3='Choose an option:'
+	options=("MySQL" "MSSQL" "PostGres" "Mongoose" "Go menu =>")
+	  select opt in "${options[@]}"
+	    do
+	      case $opt in
+	        "MySQL")
+	          	#Do any want
+	        ;;
+	        "MSSQL")
+	          	# Do whatever
+	        ;;
+	        "PostGres")
+	          	# Do whatever
+	        ;;
+	        "Mongoose")
+			
+			;;
+	        "Go menu =>")
+				menu_nodar
+			;;
+	        *) echo "[Err]Choice invalid - $REPLY";;
+	      esac
+	  done
+};
+menu_install_orm() {
+	echo -e "\e[1m[ORM Sytem]\e[0m"
+	echo -e "\e[1mInstall your ORM system."
+	echo ""
+
+	PS3='Choose an option:'
+	options=("Sequelize" "PersistenceJS" "CaminteJS" "Go menu =>")
+	  select opt in "${options[@]}"
+	    do
+	      case $opt in
+	        "Sequelize")
+	          	#Do any want
+	        ;;
+	        "PersistenceJS")
+	          	# Do whatever
+	        ;;
+	        "CaminteJS")
+	          	# Do whatever
+	        ;;
+	        "Go menu =>")
+				menu_nodar
+			;;
+	        *) echo "[Err]Choice invalid - $REPLY";;
+	      esac
+	  done
+};
+
+menu_install_front() {
+	echo -e "\e[1m[Front End Framework]\e[0m"
+	echo -e "\e[1mInstall your favorite Framework."
+	echo ""
+
+	PS3='Choose an option:'
+	options=("Angular" "Vue" "React" "Go menu =>")
+	  select opt in "${options[@]}"
+	    do
+	      case $opt in
+	        "Angular")
+	          	#Do any want
+	        ;;
+	        "Vue")
+	          	# Do whatever
+	        ;;
+	        "React")
+	          	# Do whatever
+	        ;;
+	        "Go menu =>")
+				menu_nodar
+			;;
+	        *) echo "[Err]Choice invalid - $REPLY";;
+	      esac
+	  done
+};
+
+menu_nodar() {
+	echo -e "\e[1m[Main Menu]\e[0m"
+	echo -e "\e[1mWelcome on Nodar menu lets explore."
+	echo ""
+	echo -e "\e[31mVerify your current Node.JS version before other installation..\e[0m"
+	echo ""
+
+	PS3='Choose an option:'
+	options=("Tools Manager" "Front" "Back" "Data" "ORM" "Quit")
+	  select opt in "${options[@]}"
+	    do
+	      case $opt in
+	        "Tools Manager")
+	          	menu_install_tools
+	        ;;
+	        "Front")
+	          	menu_install_front
+	        ;;
+	        "Back")
+	          	menu_install_back
+	        ;;
+	        "Data")
+	          	menu_install_data
+	        ;;
+	        "ORM")
+	        	menu_install_orm
+	        ;;
+	        "Quit")
+				break
+			;;
+	        *) echo "[Err]Choice invalid - $REPLY";;
+	      esac
+	  done
+	exit 0
+}
+menu_nodar
+
+
+
+
